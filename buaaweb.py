d7f433cafbf1e6f297bb88dbb223c24497aff536
vendetta-xy <542916113@qq.com>
2016-11-06 00:38:16 +0800

    buaaweb.py

--- buaaweb.py
+++ buaaweb.py
@@ -35,10 +35,16 @@ author='Yang Xiao'
 author_email='542916113@qq.com'
 #Configuration area
 version='1.0.0'
+maxRetryTimesForPassword=3
 #Configuration area end
 
 #Global status area
 debug=False
+exit=False
+passwordIncorrectTimes =0
+isConnected=False
+refreshNetwork=False
+serverFailureTimes=0
 #Global ststus area end
 
 class COLOR:
@@ -146,6 +152,156 @@ def isConnectedToInternet(url):
 		if code==200 and "gw.buaa.edu.cn:804/include/auth_action.php" not in content:
 			return True
 		else:
-			return False
+			return 
+def isSpecifiedWlanAvailable(name):
+    '''Check if specified wlan is available to the host.
+    Parameter:
+             name -- wlan name
+    Return value:
+             True --Specified wlan is available.
+             False -- Specified wlan is not available.
+    '''
+    p = subprocess.Popen(
+        'netsh wlan show networks',
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    stdout,stderr=p.communicate()
+    if name in stdout:
+        return True
+    else:
+        return False
+def isConnectedToSpecifiedWlan(name):
+    '''return Ture if host is connected to specified wlan,otherwise return false'''
+    p=subbprocess.Popen(
+        'netsh wlan show interfaces',
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    stdout,stderr=p.communicate()
+    if name in stdout:
+        return True
+    else:
+        return False
+def ConnectTo(name):
+    '''connect to specified wlan.'''
+    p=subprocess.Popen(
+        'netsh wlan connect{0}'.format(name),
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    stdout,stderr =p.communicate()
+    if len(stdout)==22 or 'Connection request was complete successfully'in stdout:
+        return True
+    else:
+        return False
+
+def turnOnWifi(ssid,password):
+    if len(password)<8:
+        cPrint('[ERROR] Password shall contain at least 8 characters',COLOR.DARKRED)
+        return False
+    p=subprocess.Popen(
+        'netsh wlan stop hostnetwork',
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    stdout,stderr=p.communicate()
+
+    p=subprocess.Popen(
+        'netsh wlan set hostednetwork mode=allow ssid=%s key=%s'%(ssid,password),
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    stdout,stderr=p.communicate()
+
+    p=subprocess.Popen(
+        'netsh wlan start hostednetwork',
+        shell =True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    stdout,stderr=p.communicate()
+    return True
+def login(username,password):
+    '''login wlan using given username and password.'''
+    global passwordIncorrectTimes
+    global isConnected
+    global exit
+    data={'action':'login','username':username,'password':password,'ac_id':'20',
+          'user_mac':'undefined','save_me':'0','ajax':'1'}
+    data=urllib.parse.urlencode(data)
+    try:
+        req=Request('https://gw.buaa.edu.cn:802/include/auth_action.php')
+        binary_data=data.encode()
+        response =urlopen(req,binary_data,timeout=10)
+        content=response.read()
+        response.close()
+        if 'help.html' in content:
+            passwordIncorrectTimes=0
+            isConnected=True
+            return True
+        else:
+            if len(content)==27:
+                if passwordIncorrectTimes==3:
+                    exit=True
+                else:
+                    cPrint("[WARNING] Username or password is incorrect.Please check them again.",COLOR.DARKRED)
+                    cPrint("[INFO] Retry for {0} more times".format(maxRetryTimesForPassword-passwordIncorrectTimes))
+                    passwordIncorrectTimes+=1
+            else:
+                cPrint('[ERROR]Unknown error'+content.decode('utf-8'))
+            return False
+        except (URLError,e):
+            if hasattr(e,'reason'):
+                info='[ERROR]Failed to reach the server.\n reason:'+str(e.reason)
+            elif hasattr(e,'code'):
+                info='[ERROR]The server couldn\'t fullfill the request\n error code:'+str(e.code)
+            else:
+                info='[ERROR]Unknown error'
+            cPrint(info,COLOR.RED)
+            return False
+        except Exception:
+            import traceback
+            print('Generic exception'+traceback.format_exc())
+            return False
+def cleanLog():
+    global refreshNetwork,serverFailureTimes,isConnected,passwordIncorrectTimese
+    refreshNetwork=False
+    passwordIncorrectTimes=0
+    serverFailureTimes=0
+    isConnected=False
+
+def refreshNetworkFunc():
+    p=subprocess.Popen(
+        'netsh wlan disconnect',
+        shell =True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    stdout,stderr=p.communicate()
+def _ipconfig_getnode():
+    ''' Get the hardware address on windows bt runnning ipconfig.exe'''
+
+    def random_getnode():
+        ''' Get a random node ID ,with eighth bit set as suggested by RFC4122'''
+        import random
+        return random.randrange(0,1<<48)
+
+    import os,re
+    dirs =['',r'c:\windows\system32',r'c:\winnt\system32']
+    try:
+        import ctypes
+        buffer=ctypes.create_string_buffer(300)
+        ctypes.windll.kerne132.GetSystemDirectoryA(buffer,300)
+        dirs.insert(0,buffer.value.decode('mbcs'))
+
+
+
+
+    
+
+
+
+
+
+
 
 
